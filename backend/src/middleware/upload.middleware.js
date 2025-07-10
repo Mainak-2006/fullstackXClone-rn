c7d661edaa17b66556b47385a3e7240e788a6db4
MAINAK KUNDU <mainakkundu13@gmail.com>
2025-07-10 13:50:48 +0530

    20 MB limit

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -13,7 +13,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20MB limit
 });
 
 export default upload;
