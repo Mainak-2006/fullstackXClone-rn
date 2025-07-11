3a5af0b53daeae3fb7856b29ad2553af58fdb451
MAINAK KUNDU <mainakkundu13@gmail.com>
2025-07-11 21:42:55 +0530

    user added to mongodb

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,14 +2,20 @@ import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from 'expo-router'
 import "../global.css"
+import {QueryClient,QueryClientProvider}from "@tanstack/react-query"
 
 export default function RootLayout() {
+
+  const queryClient= new QueryClient()
+  
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack>
+      <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{headerShown:false}}>
       <Stack.Screen name="(auth)" options={{headerShown:false}}/>
       <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
       </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   )
 }
