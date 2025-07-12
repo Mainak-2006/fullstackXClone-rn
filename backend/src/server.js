1a4525ea69f97480d393ecc636c4c6c3b78afe62
MAINAK KUNDU <mainakkundu13@gmail.com>
2025-07-12 09:09:07 +0530

    arcjet redeployed

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ import notificationRoutes from "./routes/notification.route.js";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 
 app.get("/", (req, res) => res.send("Hello from X Server"));
