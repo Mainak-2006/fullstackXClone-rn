74717e4346f03d7f3367b6feca3476d60086f169
MAINAK KUNDU <mainakkundu13@gmail.com>
2025-07-14 20:19:24 +0530

    added feed ,comment and like functionability

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,20 +1,30 @@
 import PostComposer from "@/components/PostComposer";
+import PostsList from "@/components/PostsList";
 import SignOutButton from "@/components/SignOutButton";
+import { usePosts } from "@/hooks/usePosts";
 import { useUserSync } from "@/hooks/useUserSync";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { useState } from "react";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { RefreshControl, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const [isRefetching, setIsRefetching] = useState(false);
+  const { refetch: refetchPosts } = usePosts();
 
+  const handlePullToRefresh = async () => {
+    setIsRefetching(true);
+
+    await refetchPosts();
+    setIsRefetching(false);
+  };
 
   useUserSync();
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
-      <FontAwesome6 name="x-twitter" size={24} color="black" />
+        <FontAwesome6 name="x-twitter" size={24} color="black" />
         <Text className="text-xl font-bold text-gray-900">Home</Text>
         <SignOutButton />
       </View>
@@ -23,8 +33,16 @@ const HomeScreen = () => {
         showsVerticalScrollIndicator={false}
         className="flex-1"
         contentContainerStyle={{ paddingBottom: 80 }}
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefetching}
+            onRefresh={handlePullToRefresh}
+            tintColor={"#1DA1F2"}
+          />
+        }
       >
         <PostComposer />
+        <PostsList />
       </ScrollView>
     </SafeAreaView>
   );
